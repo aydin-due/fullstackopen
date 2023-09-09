146df72c5c7066949807666c3c3a4e95522abda9
aydin :^) <a219201085@unison.mx>
2023-09-09 12:50:52 -0700

    1c - functions as event handlers

--- part1/lessons/c/src/App.jsx
+++ part1/lessons/c/src/App.jsx
@@ -3,14 +3,14 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  
   console.log('rendering...', count)
-
+  const increase = () => setCount(count+1)
+  const reset = () => setCount(0)
   return (
     <>
       {count}
-      <button onClick={()=>setCount(count+1)}>+</button>
-      <button onClick={()=>setCount(0)}>reset</button>
+      <button onClick={increase}>+</button>
+      <button onClick={reset}>reset</button>
     </>
   )
 }
