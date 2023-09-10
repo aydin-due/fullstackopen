4de61ef5ba425143708eb94214199ea8a3f42868
aydin :^) <a219201085@unison.mx>
2023-09-10 00:10:19 -0700

    1d - arrays

--- part1/lessons/d/src/App.jsx
+++ part1/lessons/d/src/App.jsx
@@ -2,32 +2,32 @@ import { useState } from 'react'
 import './App.css'
 
 const App = () => {
-const [clicks, setClicks] = useState({left: 0, right: 0})
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
 
-const handleLeftClick = () => {
-  setClicks({
-    ...clicks,
-    left: clicks.left + 1,
-  })
-}
-
-const handleRightClick = () => {
-  setClicks({
-    ...clicks,
-    right: clicks.right + 1,
-  })
-}
+  const handleLeftClick = () => {
+    // can also just push method but it's not recomemended since it mutates the original array
+    // whereas concat method returns a copy of the array + the new item
+    setAll(allClicks.concat('L'))
+    setLeft(left+1)
+  }
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setLeft(right+1)
+  }
 
   return (
     <div>
-      {clicks.left}
+      {left}
       <button onClick={handleLeftClick}>
         left
       </button>
       <button onClick={handleRightClick}>
         right
       </button>
-      {clicks.right}
+      {right}
+      <p>{allClicks.join(' ')}</p>
     </div>
   )
 }
