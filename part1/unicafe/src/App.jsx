dc1161c00f956662d9a88adc3c8d52e9389b76ef
aydin :^) <a219201085@unison.mx>
2023-09-10 01:44:38 -0700

    1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -7,10 +7,25 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
 
-  const setGoodFeedback = () => setGood(good+1)
-  const setNeutralFeedback = () => setNeutral(neutral+1)
-  const setBadFeedback = () => setBad(bad+1)
+  const setGoodFeedback = () => { 
+    setGood(good+1)
+    setTotal(total+1)
+    
+  }
+  const setNeutralFeedback = () => {
+    setNeutral(neutral+1)
+    setTotal(total+1)
+  }
+    const setBadFeedback = () => {
+      setBad(bad+1)
+      setTotal(total+1)
+    }
+
+    const calculateScore = () => total == 0 ? 0 : (good - bad) / total;
+
+    const positivePercentage = () => total == 0 ? 0 : (good*100)/total
 
   return (
     <div>
@@ -24,6 +39,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {calculateScore()}</p>
+      <p>positive {positivePercentage()}%</p>
     </div>
   )
 }
