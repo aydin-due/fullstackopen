61f6ba97eff52c0c653126fb8b5291061bd07019
aydin :^) <a219201085@unison.mx>
2023-09-10 00:02:00 -0700

    1d - complex state

---
+++ part1/lessons/d/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import './App.css'
+
+const App = () => {
+const [clicks, setClicks] = useState({left: 0, right: 0})
+
+const handleLeftClick = () => {
+  const newClicks = {
+    left: clicks.left + 1,
+    right: clicks.right
+  }
+  setClicks(newClicks)
+}
+
+const handleRightClick = () => {
+  const newClicks = {
+    left: clicks.left,
+    right: clicks.right + 1,
+  }
+  setClicks(newClicks)
+}
+
+  return (
+    <div>
+      {clicks.left}
+      <button onClick={handleLeftClick}>
+        left
+      </button>
+      <button onClick={handleRightClick}>
+        right
+      </button>
+      {clicks.right}
+    </div>
+  )
+}
+export default App
