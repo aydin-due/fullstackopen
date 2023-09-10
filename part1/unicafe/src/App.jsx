f168a70e2355259ed59a7b0788f6af686b31f176
aydin :^) <a219201085@unison.mx>
2023-09-10 00:49:40 -0700

    1.6

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      code here
+    </div>
+  )
+}
+
+export default App
