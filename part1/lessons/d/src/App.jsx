d2c1f010830c0dbbcf0586ff0e72aaf539df2b13
aydin :^) <a219201085@unison.mx>
2023-09-10 00:16:34 -0700

    1d - asynchronous state update

--- part1/lessons/d/src/App.jsx
+++ part1/lessons/d/src/App.jsx
@@ -5,16 +5,24 @@ const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
   const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
 
   const handleLeftClick = () => {
     // can also just push method but it's not recomemended since it mutates the original array
     // whereas concat method returns a copy of the array + the new item
     setAll(allClicks.concat('L'))
-    setLeft(left+1)
+    // set state is asynchronous, so it doesn't update instantly
+    // it's best to declare a new value and set it manually than to wait for the update in other components
+    // bc it might not render w/ the updated value
+    const updatedLeft = left+1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft+right)
   }
   const handleRightClick = () => {
     setAll(allClicks.concat('R'))
-    setLeft(right+1)
+    const updatedRight = right+1
+    setRight(updatedRight)
+    setTotal(left+updatedRight)
   }
 
   return (
@@ -28,6 +36,7 @@ const App = () => {
       </button>
       {right}
       <p>{allClicks.join(' ')}</p>
+      <p>total {total}</p>
     </div>
   )
 }
