133e7ce1c3438bd14d3fa1ed698f7aea1b1d6cbb
aydin :^) <a219201085@unison.mx>
2023-09-10 11:08:29 -0700

    1a - key attribute

--- part2/lessons/a/src/App.jsx
+++ part2/lessons/a/src/App.jsx
@@ -5,7 +5,9 @@ const App = (props) => {
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note => <li>{note.content}</li>)}
+        {notes.map(note => 
+          <li key={note.id}>{note.content}</li>
+        )}
       </ul>
     </div>
   )
