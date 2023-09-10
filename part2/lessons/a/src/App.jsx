05df734b0f30f2bce6cdd06d07e4df2ca127f3bf
aydin :^) <a219201085@unison.mx>
2023-09-10 11:06:01 -0700

    2a - rendering collections

---
+++ part2/lessons/a/src/App.jsx
@@ -0,0 +1,14 @@
+const App = (props) => {
+  const { notes } = props
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => <li>{note.content}</li>)}
+      </ul>
+    </div>
+  )
+}
+
+export default App
