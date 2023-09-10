80f5c2b288fd4f55a24b5a57f8fe994146e31a3a
aydin :^) <a219201085@unison.mx>
2023-09-10 11:56:17 -0700

    2.3

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -13,8 +13,7 @@ const Content = ({parts}) => parts.map(part => <Part key={part.id} part={part} /
 const Part = ({part}) => <p>{part.name} {part.exercises}</p>
 
 const Total = ({parts}) => {
-    let total = 0
-    parts.map(part => total += part.exercises)
+    const total = parts.reduce((sum, part) => sum + part.exercises, 0)
     
     return (
        <p><strong>total of {total} exercises</strong></p>
