2334bcfb1ebf06cffe002bea737b761246a997c1
aydin :^) <a219201085@unison.mx>
2023-09-12 11:51:10 -0700

    2e - adding styles

--- part2/lessons/notes/src/components/Note.jsx
+++ part2/lessons/notes/src/components/Note.jsx
@@ -1,7 +1,7 @@
 const Note = ({note, toggleImportance}) => {
     const label = note.important ? 'mark as not important' : 'mark as important'
     return (
-        <li>
+        <li className="note">
             {note.content}
             <button onClick={toggleImportance}>{label}</button>
         </li>
