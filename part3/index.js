44642e349ef83ded060c1cf62e0ae84042bc9213
aydin :^) <a219201085@unison.mx>
2023-09-17 11:29:20 -0700

    3a - receiving data

--- part3/index.js
+++ part3/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 let notes = [
     {
@@ -42,6 +43,30 @@ app.delete('/api/notes/:id', (req, res) => {
   notes = notes.filter(note => note.id !== id)
   // 204 no content
   res.status(204).end()
+})
+
+const generateID = () => {
+  const maxID = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
+  return maxID + 1
+}
+
+app.post('/api/notes', (req, res) => {
+  const body = req.body
+  
+  if (!body.content) {
+    return res.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateID()
+  }
+  
+  notes = notes.concat(note)
+  res.json(note)
 })
 
 const PORT = 3001;
