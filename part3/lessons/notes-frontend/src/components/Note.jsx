4fa878d0560ae495cc3288e5261b6ca8ad44b620
aydin :^) <a219201085@unison.mx>
2023-09-17 18:24:40 -0700

    3b - connect frontend to backend

---
+++ part3/lessons/notes-frontend/src/components/Note.jsx
@@ -0,0 +1,11 @@
+const Note = ({note, toggleImportance}) => {
+    const label = note.important ? 'mark as not important' : 'mark as important'
+    return (
+        <li className="note">
+            {note.content}
+            <button onClick={toggleImportance}>{label}</button>
+        </li>
+    )
+}
+
+export default Note
