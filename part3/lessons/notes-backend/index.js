293988ebd43a96ab1414c5871f28065355bf5781
aydin :^) <a219201085@unison.mx>
2023-09-18 10:01:30 -0700

    3b - serving static files from the backend

--- part3/lessons/notes-backend/index.js
+++ part3/lessons/notes-backend/index.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 let notes = [
     {
@@ -71,7 +72,7 @@ app.post('/api/notes', (req, res) => {
   res.json(note)
 })
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
