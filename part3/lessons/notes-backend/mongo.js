0558ee949432926896f56c1765457c66538865fa
aydin :^) <a219201085@unison.mx>
2023-09-25 20:55:43 -0700

    3.12

--- part3/lessons/notes-backend/mongo.js
+++ part3/lessons/notes-backend/mongo.js
@@ -12,13 +12,17 @@ const url = `mongodb+srv://aydin:${password}@fso.kpdbrfn.mongodb.net/notesapp?re
 mongoose.set('strictQuery',false)
 mongoose.connect(url)
 
+// schema tells mongoose how the obj are to be stored in the db
 const noteSchema = new mongoose.Schema({
   content: String,
   important: Boolean,
 })
 
+// models are constructor functions (js obj)
 const Note = mongoose.model('Note', noteSchema)
 
+// create note
+/*
 const note = new Note({
   content: 'HTML is Easy',
   important: true,
@@ -26,5 +30,14 @@ const note = new Note({
 
 note.save().then(result => {
   console.log('note saved!')
+  mongoose.connection.close()
+})
+*/
+
+// get notes
+Note.find({}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
   mongoose.connection.close()
 })
