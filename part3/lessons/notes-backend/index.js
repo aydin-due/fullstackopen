679cd0d6516ee2bfa6081a05aa52d46a3492b73a
aydin :^) <a219201085@unison.mx>
2023-09-27 19:16:34 -0700

    3.13 & 3.14

--- part3/lessons/notes-backend/index.js
+++ part3/lessons/notes-backend/index.js
@@ -35,13 +35,7 @@ app.get('/api/notes', (req, res) => {
 })
 
 app.get('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const note = notes.find(note => note.id === id)
-  if (note) {
-    res.json(note)
-  } else {
-    res.status(404).end()
-  }
+  Note.findById(req.params.id).then(note => {res.json(note)})
 })
 
 app.delete('/api/notes/:id', (req, res) => {
@@ -51,28 +45,21 @@ app.delete('/api/notes/:id', (req, res) => {
   res.status(204).end()
 })
 
-const generateID = () => {
-  const maxID = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
-  return maxID + 1
-}
-
 app.post('/api/notes', (req, res) => {
   const body = req.body
   
-  if (!body.content) {
+  if (body.content === undefined) {
     return res.status(400).json({
       error: 'content missing'
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
-    important: body.important || false,
-    id: generateID()
-  }
+    important: body.important || false
+  })
   
-  notes = notes.concat(note)
-  res.json(note)
+  note.save().then(savedNote => {res.json(savedNote)})
 })
 
 const PORT = process.env.PORT;
