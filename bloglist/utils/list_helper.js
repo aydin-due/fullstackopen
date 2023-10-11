d431c954a2aa6b3cddfdf689c7e2d44f17cd4f8f
aydin :^) <a219201085@unison.mx>
2023-10-11 07:03:10 -0700

    4.5

--- bloglist/utils/list_helper.js
+++ bloglist/utils/list_helper.js
@@ -6,7 +6,12 @@ const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    return blogs.reduce((max, blog) => max.likes > blog.likes ? max : blog)
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
