e6fa6d396b231325cd7bfebd2b33aa350f35c472
aydin :^) <a219201085@unison.mx>
2023-10-11 07:23:35 -0700

    4.7

--- bloglist/utils/list_helper.js
+++ bloglist/utils/list_helper.js
@@ -21,9 +21,20 @@ const mostBlogs = (blogs) => {
     return { author: mostBlogsAuthor[0], blogs: mostBlogsAuthor[1] }
 }
 
+const mostLikes = (blogs) => {
+    const authorLikes = blogs.reduce((likes, blog) => {
+        likes[blog.author] = (likes[blog.author] || 0) + blog.likes
+        return likes
+    }, {})
+    const authorLikesArray = Object.entries(authorLikes)
+    const mostLikesAuthor = authorLikesArray.reduce((max, author) => max[1] > author[1] ? max : author)
+    return { author: mostLikesAuthor[0], likes: mostLikesAuthor[1] }
+}
+
 module.exports = {
     dummy,
     totalLikes,
     favoriteBlog,
-    mostBlogs
+    mostBlogs,
+    mostLikes
 }
