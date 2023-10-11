3ef51119f2b96c8b26fb0e5ff93bf53cd8ce8444
aydin :^) <a219201085@unison.mx>
2023-10-10 19:34:25 -0700

    part 4 setup

---
+++ phonebook-backend/index.js
@@ -0,0 +1,103 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const Phonebook = require('./models/phonebook')
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(express.static('dist'))
+
+morgan.token('req-body', (req) => {
+	return JSON.stringify(req.body)
+})
+
+app.use(
+	morgan(':method :url :status :res[content-length] - :response-time ms :req-body')
+)
+
+app.get('/api/persons', (req, res) => {
+	Phonebook.find({}).then(ppl => res.json(ppl))
+})
+
+app.get('/info', (req, res) => {
+	const date = new Date()
+	Phonebook.find({}).then(ppl =>
+		res.send(`<p>Phonebook has info for ${ppl.length} people</p><p>${date}</p>`))
+})
+
+app.get('/api/persons/:id', (req, res, next) => {
+	Phonebook.findById(req.params.id)
+		.then(person => {
+			if (person) {
+				res.json(person)
+			} else {
+				res.status(400).end()
+			}
+		})
+		.catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (req, res, next) => {
+	Phonebook.findByIdAndDelete(req.params.id)
+		.then(() => res.status(204).end())
+		.catch(error => next(error))
+})
+
+app.post('/api/persons', (req, res, next) => {
+	const body = req.body
+
+	const person = new Phonebook({
+		name: body.name,
+		number: body.number
+	})
+
+	/*
+    if (persons.find(p => p.name === person.name)) {
+        return res.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+    */
+
+	person.save()
+		.then(savedPerson => res.json(savedPerson))
+		.catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+	const person = {
+		name: req.body.name,
+		number: req.body.number
+	}
+
+	Phonebook.findByIdAndUpdate(req.params.id, person, { new: true, runValidators: true, context: 'query' })
+		.then(updatedPerson => res.json(updatedPerson))
+		.catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+	response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+	console.error(error.message)
+
+	if (error.name === 'CastError') {
+		return res.status(400).send({ error: 'malformatted id' })
+	} else if (error.name === 'ValidationError'){
+		return res.status(400).send({ error: error.message })
+	}
+
+	next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`)
+})
