3e1bd7eae1ca798adc1977d66fb2197f464dd613
aydin :^) <a219201085@unison.mx>
2023-10-11 21:26:02 -0700

    4b - test env

--- lessons/notes-backend/utils/config.js
+++ lessons/notes-backend/utils/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT
-let MONGODB_URI = process.env.MONGODB_URI
+let MONGODB_URI = process.env.NODE_ENV === 'test' ? process.env.TEST_MONGODB_URI : process.env.MONGODB_URI
 
 module.exports = {
 	MONGODB_URI,
